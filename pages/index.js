040d2d7b75185a44662333eb3ad6af953cd95f40
ChebanenkoAlexander <110239245+ChebanenkoAlexander@users.noreply.github.com>
2022-11-18 16:26:45 +0300

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main>
+        <h1 className="text-4xl">Welcome</h1>
+        <p>Some text</p>
+      </main>
+    </div>
+  );
+}
